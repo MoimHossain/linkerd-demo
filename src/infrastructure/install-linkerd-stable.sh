7f92eb42ee3700bd68a2065f16d5ec3ec4fc90af
Moim HA <moimhossain@gmail.com>
2021-03-11 10:01:39 +0100

    Linkerd viz moved

--- src/infrastructure/install-linkerd-stable.sh
+++ src/infrastructure/install-linkerd-stable.sh
@@ -5,3 +5,11 @@ export PATH=$PATH:$HOME/.linkerd2/bin
 linkerd check --pre
 linkerd install | kubectl apply -f -
 linkerd check
+
+
+
+linkerd viz install | kubectl apply -f -
+
+linkerd viz check
+linkerd check
+kubectl apply -f ./dashboard.yml
--- src/infrastructure/install-linkerd-viz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm repo add linkerd https://helm.linkerd.io/stable
-helm install linkerd/linkerd-viz
-linkerd viz install | kubectl apply -f -
-
-linkerd viz check
-kubectl apply -f ./dashboard.yml
