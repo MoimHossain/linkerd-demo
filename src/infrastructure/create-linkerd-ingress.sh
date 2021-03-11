175dcd91508332dda8ae420d7eaf5f21f92e7ec4
Moim HA <moimhossain@gmail.com>
2021-03-11 14:38:34 +0100

    unnecessary delay

--- src/infrastructure/create-linkerd-ingress.sh
+++ src/infrastructure/create-linkerd-ingress.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "Waiting for 2 mins to give ingress-nginx time to get things worked out..."
-sleep 2m
-kubectl get all -n ingress-nginx
-
-
 echo "Creating ingress for Linkerd viz"
 kubectl apply -f ./dashboard.yml
